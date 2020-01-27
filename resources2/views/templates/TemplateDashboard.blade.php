85b52a4624d71f9d6777291c215a36572f807bd0
ageng123 <ageng133@gmail.com>
2020-01-27 21:29:40 +0700

    Update TemplateDashboard.blade.php

--- resources2/views/templates/TemplateDashboard.blade.php
+++ resources2/views/templates/TemplateDashboard.blade.php
@@ -73,7 +73,7 @@
                 @if(Session::get('role') == '1' or Session::get('role') == '2')
                     <li><a href="{{route('Semua.index')}}"><span class="uk-margin-small-right" uk-icon="icon: table"></span> Data Pembayaran SPP</a></li>
                     <li><a href="{{route('Semua.create')}}"><span class="uk-margin-small-right" uk-icon="icon: plus"></span> Tambah Data Pembayaran</a></li>
-
+                    <li><a href="{{route('Status.index')}}"><span class="uk-margin-small-right" uk-icon="icon: history"></span> Status Pembayaran SPP</a></li>
                 @elseif(Session::get('jabatan') == 'SuperAdmin' or Session::get('nik') != null)
                     <li><a href="{{route('Semua.create')}}"><span class="uk-margin-small-right" uk-icon="icon: plus"></span> Tambah Data Pembayaran</a></li>
                     <li><a href="{{route('Semua.index')}}"><span class="uk-margin-small-right" uk-icon="icon: table"></span> Data Pembayaran SPP</a></li>
